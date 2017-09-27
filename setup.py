fa91adb1ec3d2a372eae4431376058f0314f879a
quinn <quinn@quinnweber.com>
2017-09-26 19:26:15 -0700

    using zappa instead of heroku

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='groupme-bot',
-    version='0.0.1',
+    version='0.0.2',
     description='GroupMe bot',
     author='Quinn Weber',
     maintainer='Quinn Weber',
